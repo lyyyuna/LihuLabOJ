9f45b281dc80623f82a57cc5f2a6961e4b56da03
lyyyuna <lyyyuna@gmail.com>
2017-05-02 08:33:04 +0800

    user serializers

--- LihuLabOJ/user/views.py
+++ LihuLabOJ/user/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib import auth
 
-# Create your views here.
+from rest_framework.views import APIView
+
+from serializers import UserLoginSerializer
+from common import shortcuts
+
+
+class UserLoginAPIView(APIView):
+    def post(self, request):
+        '''
+        For user login
+        '''
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            data = serializer.data
+            user = auth.authenticate(
+                        request=request
+                        username=data['username']
+                        password=data['password'])
+            if user is not None:
+                auth.login(request, user)
+                shortcuts.success_response('Login success.')
+            else:
+                shortcuts.error_response('Login failed.')
+        else:
+            shortcuts.error_response('The input cannot be accepted.')
