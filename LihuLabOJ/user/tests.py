afd83dc1e71338efc74cc5aee908f45b89a64204
lyyyuna <lyyyuna@gmail.com>
2017-05-03 22:17:56 +0800

    modify unittest

--- LihuLabOJ/user/tests.py
+++ LihuLabOJ/user/tests.py
@@ -12,24 +12,24 @@ class AccountsTestCase(TestCase):
     def test_user_login(self):
         res = self.client.post(reverse('user_login_api'), {'username': 'yigo', 'password': 'yigo'}, format='json')
         self.assertEqual(res.status_code, 200)
-        js_dic = json.loads(res.content)
+        js_dic = json.loads(res.content.decode('utf-8'))
         self.assertEqual(js_dic['code'], 0)
         self.assertEqual(js_dic['data'], 'Login success.')
 
     def test_FET_user_logout_without_login(self):
         res = self.client.get(reverse('user_logout_api'))
         self.assertEqual(res.status_code, 403)
-        js_dic = json.loads(res.content)
+        js_dic = json.loads(res.content.decode('utf-8'))
         self.assertEqual(js_dic['detail'], 'Authentication credentials were not provided.')
 
     def test_user_logout(self):
         res = self.client.post(reverse('user_login_api'), {'username': 'yigo', 'password': 'yigo'}, format='json')
         self.assertEqual(res.status_code, 200)
-        js_dic = json.loads(res.content)
+        js_dic = json.loads(res.content.decode('utf-8'))
         self.assertEqual(js_dic['code'], 0)
         self.assertEqual(js_dic['data'], 'Login success.')
         res = self.client.get(reverse('user_logout_api'))
         self.assertEqual(res.status_code, 200)
-        js_dic = json.loads(res.content)
+        js_dic = json.loads(res.content.decode('utf-8'))
         self.assertEqual(js_dic['code'], 0)
         self.assertEqual(js_dic['data'], 'Logout success.')
